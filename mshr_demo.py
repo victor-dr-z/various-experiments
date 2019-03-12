ec039a54d66f2281df0e68771822b70994544279
Weixiong Zheng <wzheng@carbon3d.com>
2019-03-12 13:27:56 -0700

    added mshr experiment

---
+++ mshr_demo.py
@@ -0,0 +1,19 @@
+from mshr import *
+import dolfin as df
+
+box = Box(df.Point(0,0,0), df.Point(1,1,1))
+c1 = Cylinder(df.Point(.5,.5,1),df.Point(.5,.5,0),.3,.3)
+c2 = Cylinder(df.Point(0,.5,.5),df.Point(1,.5,.5),.3,.3)
+domain = box - c1 -c2
+
+generator = CSGCGALMeshGenerator3D()
+generator.parameters["edge_size"] = 0.025
+generator.parameters["facet_angle"] = 25.0
+generator.parameters["facet_size"] = 0.05
+
+# Invoke the mesh generator
+#mesh = generator.generate(CSGCGALDomain3D(domain))
+mesh = generate_mesh(domain, 30)
+#dolfin.plot(mesh, "3D mesh")
+df.File('mesh.pvd') << mesh
+#domain = Rectangle(dolfin.Point(0., 0.), dolfin.Point(1., 1.)) -Circle(dolfin.Point(0.0, 0.0), .35)
